8bbc279ae3864ae53330bc22e64e15bc82c6e0b0
George Guilherme <george.gui2000@gmail.com>
2024-02-02 15:42:42 -0300

    :sparkles: feat: criado o mapping do usuario

---
+++ Infra/Data/Mappings/UserMap.cs
@@ -0,0 +1,49 @@
+﻿using Blog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Infra.Data.Mappings;
+
+public class UserMap : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.HasIndex(x => x.Email).IsUnique();
+        builder.HasIndex(x => x.Nickname).IsUnique();
+        builder.HasIndex(x => x.Cpf).IsUnique();
+
+        builder.Property(x => x.Email)
+            .HasColumnType("varchar")
+            .HasMaxLength(150)
+            .IsRequired();
+
+        builder.Property(x => x.Password)
+            .HasColumnType("varchar")
+            .HasMaxLength(256)
+            .IsRequired();
+
+        builder.Property(x => x.Nickname)
+            .HasColumnType("varchar")
+            .HasMaxLength(30)
+            .IsRequired();
+
+        builder.Property(x => x.Name)
+            .HasColumnType("varchar")
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(x => x.Cpf)
+            .HasColumnType("varchar")
+            .HasMaxLength(11)
+            .IsRequired();
+
+        builder.Property(x => x.CreatedAt)
+            .HasColumnType("datetime")
+            .IsRequired();
+
+        builder.Property(x => x.LastUpdate)
+            .HasColumnType("datetime");
+    }
+}
