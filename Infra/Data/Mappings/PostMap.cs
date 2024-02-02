6e8a52ccdfb96c39bfc44aac0260855c12ffebf7
George Guilherme <george.gui2000@gmail.com>
2024-02-02 19:31:30 -0300

    :sparkles: feat: mapping do post

---
+++ Infra/Data/Mappings/PostMap.cs
@@ -0,0 +1,31 @@
+﻿using Blog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Infra.Data.Mappings;
+
+public class PostMap : IEntityTypeConfiguration<Post>
+{
+    public void Configure(EntityTypeBuilder<Post> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(p => p.Description)
+            .HasColumnType("varchar")
+            .HasMaxLength(256)
+            .IsRequired();
+
+
+        builder.Property(x => x.CreatedAt)
+            .HasColumnType("datetime")
+            .IsRequired();
+
+        builder.Property(x => x.LastUpdate)
+            .HasColumnType("datetime");
+
+        builder.HasOne(e => e.CreatedBy)
+            .WithMany(c => c.MyPosts)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
+    }
+}
