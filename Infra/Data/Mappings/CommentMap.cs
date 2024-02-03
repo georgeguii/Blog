8f4596e9fcb2c02e001834c9e8b57a4d62126bab
George Guilherme <george.gui2000@gmail.com>
2024-02-02 21:41:00 -0300

    :sparkles: feat: mapping de comentarios e likes

---
+++ Infra/Data/Mappings/CommentMap.cs
@@ -0,0 +1,44 @@
+﻿using Blog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Infra.Data.Mappings;
+
+public class CommentMap : IEntityTypeConfiguration<Comment>
+{
+    public void Configure(EntityTypeBuilder<Comment> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(p => p.Description)    
+            .HasColumnType("varchar")
+            .HasMaxLength(256)
+            .IsRequired();
+
+
+        builder.Property(x => x.CreatedAt)
+            .HasColumnType("datetime")
+            .IsRequired();
+
+        builder.Property(x => x.LastUpdate)
+            .HasColumnType("datetime");
+
+        builder.HasOne(e => e.CreatedBy)
+            .WithMany(c => c.MyComments)
+            .HasForeignKey(e => e.UserId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
+
+        builder.HasOne(e => e.OnPost)
+            .WithMany(c => c.Comments)
+            .HasForeignKey(e => e.PostId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
+
+        builder.HasOne(e => e.RelatedComment)
+            .WithMany(c => c.ChildrenComments)
+            .HasForeignKey(e => e.CommentId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired(false);
+    }
+}
---
+++ Infra/Data/Mappings/LikeMap.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Infra.Data.Mappings;
+
+public class LikeMap : IEntityTypeConfiguration<Like>
+{
+    public void Configure(EntityTypeBuilder<Like> builder)
+    {
+        builder.HasKey(l => new { l.UserId, l.PostId });
+
+        builder.Property(x => x.CreatedAt)
+            .HasColumnType("datetime")
+            .IsRequired();
+    }
+}
