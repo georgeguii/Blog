f17c7679b7816c1f980d6d4e39b7d87845266984
George Guilherme <george.gui2000@gmail.com>
2024-02-24 18:11:12 -0300

    feat: criado postrepository

---
+++ Infra/Repositories/PostRepository.cs
@@ -0,0 +1,69 @@
+﻿using Blog.Domain.Entities;
+using Blog.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Infra.Repositories;
+
+public class PostRepository
+{
+    private readonly BlogContext _context;
+
+    public PostRepository(BlogContext context)
+    {
+        _context = context;
+    }
+
+
+    public async Task<IEnumerable<object>> GetManyAsync(
+        int? page = null,
+        int? pageSize = null)
+    {
+        var query = _context.Posts.AsQueryable();
+
+        if (page.HasValue && pageSize.HasValue && page > 0 && pageSize > 0)
+        {
+            query = query.Skip((page.Value - 1) * pageSize.Value)
+                .Take(pageSize.Value);
+        }
+
+        return await query.Select(x => new
+        {
+            x.Id,
+            x.Description,
+            x.CreatedBy,
+            x.CreatedAt,
+            x.LastUpdate
+        }).ToListAsync();
+    }
+
+    public async Task<Post?> GetOneAsync(int id) =>
+        await _context.Posts.FindAsync(id);
+
+    public async Task CreateAsync(Post post)
+    {
+        await _context.Posts.AddAsync(post);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<bool> UpdateAsync(Post post)
+    {
+        var updated = await _context
+            .Posts
+            .Where(x => x.Id == post.Id)
+            .ExecuteUpdateAsync(x => x
+                .SetProperty(p => p.Description, post.Description)
+                .SetProperty(p => p.LastUpdate, post.LastUpdate));
+
+        return updated != 0;
+    }
+
+    public async Task<bool> DeleteAsync(Post post)
+    {
+        var deleted = await _context
+            .Posts
+            .Where(x => x.Id == post.Id)
+            .ExecuteDeleteAsync();
+
+        return deleted != 0;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,8 +3,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks();
@@ -17,7 +15,6 @@ builder.Services.AddDbContextPool<BlogContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
