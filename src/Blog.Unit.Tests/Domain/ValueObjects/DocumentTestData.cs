428d5c566d0ecd9fca79505163137e3bd217082b
Joao-Kalin <vasconcelosjoao438@gmail.com>
2024-09-01 16:06:39 -0300

    feat: Add unit tests for Document class

---
+++ src/Blog.Unit.Tests/Domain/ValueObjects/DocumentTestData.cs
@@ -0,0 +1,42 @@
+﻿namespace Blog.Unit.Tests.Domain.ValueObjects;
+
+public class DocumentTestData
+{
+    public static IEnumerable<object[]> ValidCpfCases => new List<object[]>
+    {
+        new object[] { "745.425.380-60", "74542538060" },
+        new object[] { "74542538060", "74542538060" },
+        new object[] { "74542.-./538060", "74542538060" }
+    };
+
+    // CNPJ Test Cases
+    public static IEnumerable<object[]> ValidCnpjCases => new List<object[]>
+    {
+        new object[] { "12.345.678/0001-95", "12345678000195" },
+        new object[] { "12345678000195", "12345678000195" },
+        new object[] { "1234567.-./8000195", "12345678000195" }
+    };
+
+    // Invalid Document Test Cases
+    public static IEnumerable<object[]> InvalidDocumentCases => new List<object[]>
+    {
+        new object[] { "12345678900033300" },
+        new object[] { "123456784" }
+    };
+
+    // Invalid CPF Test Cases
+    public static IEnumerable<object[]> InvalidCpfCases => new List<object[]>
+    {
+        new object[] { "692.618.740-11" },
+        new object[] { "69261874011" },
+        new object[] { "00000000000" }
+    };
+
+    // Invalid CNPJ Test Cases
+    public static IEnumerable<object[]> InvalidCnpjCases => new List<object[]>
+    {
+        new object[] { "57.241.960/0001-61" },
+        new object[] { "57241960000161" },
+        new object[] { "00000000000000" }
+    };
+}
---
+++ src/Blog.Unit.Tests/Domain/ValueObjects/DocumentTests.cs
@@ -0,0 +1,69 @@
+﻿using Blog.Api.Domain.ValueObjects;
+using Blog.Api.Shared.Exceptions;
+using FluentAssertions;
+
+namespace Blog.Unit.Tests.Domain.ValueObjects;
+
+public class DocumentTests
+{
+
+    [Theory]
+    [MemberData(nameof(DocumentTestData.ValidCpfCases), MemberType = typeof(DocumentTestData))]
+    public void ShouldCreateDocumentWithValidCpf(string input, string expectedText)
+    {
+        // Act
+        var document = (Document)input;
+
+        // Assert
+        document.Type.Should().Be(DocumentType.CPF);
+        document.Text.Should().Be(expectedText);
+    }
+    
+    [Theory]
+    [MemberData(nameof(DocumentTestData.ValidCnpjCases), MemberType = typeof(DocumentTestData))]
+    public void ShouldCreateDocumentWithValidCnpj(string input, string expectedText)
+    {
+        // Act
+        var document = (Document)input;
+
+        // Assert
+        document.Type.Should().Be(DocumentType.CNPJ);
+        document.Text.Should().Be(expectedText);
+    }
+
+    [Theory]
+    [MemberData(nameof(DocumentTestData.InvalidDocumentCases), MemberType = typeof(DocumentTestData))]
+    public void ShouldThrowExceptionForInvalidDocument(string input)
+    {
+        // Act
+        var action = () => (Document)input;
+
+        // Assert
+        action.Should().Throw<DomainException>()
+            .WithMessage("Documento inválido");
+    }
+    
+    [Theory]
+    [MemberData(nameof(DocumentTestData.InvalidCpfCases), MemberType = typeof(DocumentTestData))]
+    public void ShouldThrowExceptionForInvalidCpf(string input)
+    {
+        // Act
+        var action = () => (Document)input;
+
+        // Assert
+        action.Should().Throw<DomainException>()
+            .WithMessage($"{DocumentType.CPF} inválido");
+    }
+
+    [Theory]
+    [MemberData(nameof(DocumentTestData.InvalidCnpjCases), MemberType = typeof(DocumentTestData))]
+    public void ShouldThrowExceptionForInvalidCnpj(string input)
+    {
+        // Act
+        var action = () => (Document)input;
+
+        // Assert
+        action.Should().Throw<DomainException>()
+            .WithMessage($"{DocumentType.CNPJ} inválido");
+    }
+}
